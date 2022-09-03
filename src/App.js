dc03873877f30a3755c85b8a777aa450b87e4c0b
waltergarcia <waltez913@gmail.com>
2022-09-02 20:56:59 -0600

    Added logic to search images from unsplash API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+import './content.css'
+import './article.css'
 
-function App() {
+const API_URL = 'https://api.unsplash.com/search/photos?per_page=20&query='
+const API_KEY = 'Client-ID vJ_uTHt8m8YJgkYrx6suLyHO5g-NBzHVd6KPgFMj1_M'
+
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            const response = await fetch(API_URL + values.search, {
+              headers: {
+                'Authorization': API_KEY
+              }
+            })
+            const data = await response.json()
+            setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" placeholder='Type a word'/>
+          </Form>
+        </Formik>
       </header>
+      <div className='contaier'>
+        <div className='center'>
+          {photos.map(photo =>
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt='Not Found' />
+              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+            </article>
+          )}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
